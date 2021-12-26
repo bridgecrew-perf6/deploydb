80916228618b9716ff8d1ba53f080513766ed62a
lavsri2020 <86874286+lavsri2020@users.noreply.github.com>
2021-12-26 15:48:41 +0530

    added

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
-
 const router = require('./Router/index');
+const cors=require('cors');
 
 const app = express();
 
+const localDBUrl = 'mongodb://127.0.0.1:27017/db_edureka';
 
-const localDBUrl='mongodb://127.0.0.1:27017/db_edureka';
 
-
-app.use(cors());
 app.use(express.json());
-app.use('/api', router);
+app.use('api/', router);
 
 mongoose.connect(process.env.SERVER_URL, {
     useNewUrlParser: true, useUnifiedTopology: true
 })
     .then(res => {
-        app.listen(process.env.PORT, () => {
+        app.listen(process.env.PORT,() => {
             console.log(`Server is running `)
         });
     })
