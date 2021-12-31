2efb53947515d221ecba707d0e5de2e486849edc
lavsri2020 <86874286+lavsri2020@users.noreply.github.com>
2021-12-31 19:08:11 +0530

    added

--- app.js
+++ app.js
@@ -13,7 +13,33 @@ mongoose.connect(process.env.SERVER_URL, {
     useNewUrlParser: true, useUnifiedTopology: true,
    
 })
-    .then(res => {
+    .then(res => {const express = require('express');
+    const mongoose = require('mongoose');
+    const cors=require('cors');
+    
+    const router = require('./Router/index');
+    
+    const app = express();
+    app.use(cors());
+    const port =process.env.PORT || 1910;
+    const hostname = '0.0.0.0/0';
+    const DBUrl = 'mongodb://127.0.0.1:27017/db_edureka';
+    const atlasDBUrl = 'mongodb+srv://lavanya:lav2020@cluster0.soajy.mongodb.net/db_edureka?authSource=admin&replicaSet=atlas-95f5rx-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+    
+    app.use(express.json());
+    app.use('/api', router);
+    
+    mongoose.connect(atlasDBUrl, {
+        useNewUrlParser: true, useUnifiedTopology: true
+    })
+        .then(res => {
+            app.listen(port, hostname, () => {
+                console.log(`Server is running at ${hostname}:${port}`)
+            });
+        })
+        .catch(err => console.log(err));
+    
+    
         app.listen(process.env.PORT,() => {
             console.log(`Server is running `)
         });
